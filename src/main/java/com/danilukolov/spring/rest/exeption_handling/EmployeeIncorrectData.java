5f199214228e13a733cbcbb3da8c3528e9d07789
Ukolov1703 <ukollov@inbox.ru>
2023-06-21 20:24:44 +0700

    REST_App
    REST_App

---
+++ src/main/java/com/danilukolov/spring/rest/exeption_handling/EmployeeIncorrectData.java
@@ -0,0 +1,16 @@
+package com.danilukolov.spring.rest.exeption_handling;
+
+public class EmployeeIncorrectData {
+    private String info;
+
+    public EmployeeIncorrectData() {
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
---
+++ src/main/java/com/danilukolov/spring/rest/exeption_handling/NoSuchEmployeeException.java
@@ -0,0 +1,8 @@
+package com.danilukolov.spring.rest.exeption_handling;
+
+public class NoSuchEmployeeException extends RuntimeException {
+
+    public NoSuchEmployeeException(String message) {
+        super(message);
+    }
+}
